8628564eda40224ea45616693b28e9882b73dd2c
Tomoya Bansho <tomomail1997@gmail.com>
2021-12-09 18:09:00 +0900

    依存関係を整理しました

--- lib/google_recruit_problem.rb
+++ lib/google_recruit_problem.rb
@@ -1,34 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "google_recruit_problem/version"
-
-class UndefinedPrimarityTestMethod < StandardError; end
-
-class Integer
-  def prime?(method='ortho')
-    if ['ortho', 'fermat'].include?(method)
-      return self.public_send(method, self)
-    else
-      raise UndefinedPrimarityTestMethod.new("#{method} is not defined method")
-    end
-  end
-
-  def ortho(n)
-    (2...Math::sqrt(n).to_i+1).each do |value|
-      if n % value == 0 ; return false end
-    end
-    return true
-  end
-
-  def fermat(n)
-    if n == 2 ; return true end
-    if n == 3 ; return true end
-    (2...n).to_a.sample(n).each do |a|
-      if n.gcd(a) != 1 or a.pow(n - 1, n).to_i % n != 1 ; return false end
-    end
-    return true
-  end
-end
+require "primarity"
 
 module GoogleRecruitProblem
   class Error < StandardError; end
